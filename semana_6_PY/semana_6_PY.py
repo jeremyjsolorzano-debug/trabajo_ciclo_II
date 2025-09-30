7934588e15ce859f874762ddeac8407cd0c3b07a
jeremy <jeremy.jsolorzano@gmail.com>
2025-09-30 14:51:01 -0500

    subienda esto

--- semana_6_PY/semana_6_PY.py
+++ semana_6_PY/semana_6_PY.py
@@ -1,1 +1,20 @@
+g = input("genere una contraseña: ")
 
+print("\n-----------------------------")
+print("|     Valida tu contraseña    |")
+print("|   1. ud. tiene 3 intentos   |")
+print("-----------------------------\n")
+
+intentos = 3
+
+while(intentos > 0):
+    c = input("ingrese la contraseña: ")
+
+    if g == c:
+        print("\nAcceso concedido. Bienvenido al sistema.")
+        break
+    else:
+        intentos-=1
+        print("contraseña incorreta. Sus intentos restantes son: ",intentos)
+       
+else: print("Acceso denegado! Cerrando sistema.")
